757c3daa24c80df0548dcf4e173041c561b53da3
David Neale <neale.dj@gmail.com>
2015-05-27 11:09:53 +0100

    Patch the PythonSerializer.handle_fk_field method to backport a fix added in Django 1.8
    The patch is only applied for Django versions <1.8.
    
    This fixes serialization of RelatedSetField fields containing a list of FKs.
    
    Backport of Django commit 5c995dcfc251b55284e1ef16545acd2acad6be04

--- djangae/patches/__init__.py
+++ djangae/patches/__init__.py
@@ -1,3 +1,5 @@
+from distutils.version import StrictVersion
+import django
 from django.conf import settings
 
 
@@ -5,3 +7,7 @@ def patch():
     if 'django.contrib.contenttypes' in settings.INSTALLED_APPS:
         from . import contenttypes
         contenttypes.patch()
+
+    if StrictVersion(django.get_version()) < StrictVersion('1.8'):
+        from . import serialization
+        serialization.patch()
